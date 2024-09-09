681084e8c69f8a687f8a275bee893adc483a6f33
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2024-09-09 13:58:53 +0800

    Update ArrayHelper.hpp

--- include/ArrayHelper.hpp
+++ include/ArrayHelper.hpp
@@ -17,14 +17,6 @@ public:
 template<>
 class ArrayHelper<arrow::StringArray, std::string> {
 public:
-
-  /**
-   * TODO: Maybe this should return a pointer?
-   *
-   * @param array
-   * @param index
-   * @return
-   */
   static std::string at(const arrow::StringArray &array, int index) {
     return array.GetString(index);
   }
